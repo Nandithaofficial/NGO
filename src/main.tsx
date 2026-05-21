689c6e977de538a9568831551058fa0df10e24f4
Nanditha <nanditha.kallu2023@gmail.com>
2026-05-21 13:26:22 +0530

    All files uploaded

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App.tsx";
+// @ts-ignore: allow CSS side-effect import without type declarations
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
